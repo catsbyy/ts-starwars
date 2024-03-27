e32691e273ec808858964ef0169e75b1fd731084
v-andrusenko <andrusenko.vp1@gmail.com>
2024-03-27 23:58:55 +0200

    learning-ts

---
+++ starwars-project/src/App.tsx
@@ -0,0 +1,11 @@
+import { StarWars } from "./StarWars";
+
+function App() {
+  return (
+    <div className='App'>
+      <StarWars/>
+    </div>
+  )
+}
+
+export default App
---
+++ starwars-project/src/StarWars.tsx
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from "react";
+
+interface Character {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: any[];
+  starships: any[];
+  created: string;
+  edited: string;
+  url: string;
+}
+
+export const StarWars: React.FC = () => {
+  const api_base: string = "https://swapi.dev/api";
+  const api_endpoint: string = "/people/";
+
+  const [input, setInput] = useState<number>();
+
+  const [character, setCharacter] = useState<Character>();
+
+  const [totalCount, setTotalCount] = useState<number>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(api_base + api_endpoint);
+        const data = await response.json();
+        setTotalCount(data.count);
+        console.log(data.count);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const getCharacter = async () => {
+    const response = await fetch(api_base + api_endpoint + input);
+    const parsedData = await response.json();
+    setCharacter(parsedData);
+  };
+
+  useEffect(() => {
+    console.log(character);
+  }, [character]);
+
+  const handleClick = async () => {
+    await getCharacter();
+  };
+
+  return (
+    <div className="main-container">
+      <h1>Star Wars character</h1>
+      {character &&
+        Object.entries(character)
+          .slice(0, 8) // Slice the array to include only the first 8 entries
+          .map(([key, value]) => (
+            <div key={key} className="character-key">
+              <span>{key}: </span>
+              {Array.isArray(value) ? (
+                <ul>
+                  {value.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+              ) : (
+                <span className="character-value">{value}</span>
+              )}
+            </div>
+          ))}
+      <input
+        placeholder="Enter number"
+        type="number"
+        value={input}
+        onChange={(e) => setInput(Number(e.currentTarget.value))}
+      />
+      {totalCount &&
+        <div className="total-characters">
+            there are {totalCount} characters
+        </div>
+      }
+
+      <button onClick={handleClick}>Use Force!</button>
+    </div>
+  );
+};
